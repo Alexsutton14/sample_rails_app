0df45fe72105bcd7c54d4af9156f21793398e93b
Alex Sutton <alexsutton14@hotmail.com>
2020-05-26 16:35:46 +0100

    Completed 10.3.1

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,5 +39,16 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test "should get index when logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_template "index"
+  end
   
 end
