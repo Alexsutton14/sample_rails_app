e989f60ed5c31bebe8ab17219d9f898d3ab99d2c
Alex Sutton <alexsutton14@hotmail.com>
2020-05-27 09:09:26 +0100

    C10 Complete Finish user edit update index and destroy

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = users(:michael)
+    @user       = users(:michael)
     @other_user = users(:archer)
   end
 
@@ -59,6 +59,29 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password",
                                             admin: true } }
     assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should destroy when logged in as admin" do
+    log_in_as(@user)
+    assert_difference 'User.count', -1 do
+      delete user_path(@other_user)
+    end
+    assert_redirected_to users_path
   end
   
 end
